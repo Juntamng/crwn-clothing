845590c55f2c46290db810870edf20ac9b043305
Shek Hang Ng <junta@sheks-mbp.lan>
2021-09-13 16:07:30 -0700

    add sign in page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import {Switch, Route} from 'react-router-dom';
 
 import './App.css';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 const HatsPage  = () => (
   <div>
     <h1>HATS PAGE</h1>
@@ -14,9 +17,11 @@ const HatsPage  = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
